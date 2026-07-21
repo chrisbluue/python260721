f1c1b414ebed3f436ceeefb3425d005f4d6ca25b
chrisbluue <chris004jun@gmail.com>
2026-07-21 15:49:57 +0900

    클래스 연습1

--- BankAccount.py
+++ BankAccount.py
@@ -11,8 +11,7 @@ class BankAccount:
     def withdraw(self, amount):
         self.balance -= amount
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.id, self.name, self.balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
